810e0abd7ea093b6236012b9f89e38255ba6edbc
diyor <diyor@gmail.com>
2021-11-25 18:36:04 +0500

    js-test2

---
+++ script.js
@@ -0,0 +1,79 @@
+var clock = +prompt('Введите время в цифрах, от 0 до 24')
+
+switch (clock) {
+    case 1 :
+        alert('час ночи')
+        break;
+    case 2 :
+        alert('2 часа ночи')
+        break;
+    case 3 :
+        alert('3 часа ночи')
+        break;
+    case 4:
+        alert('4 часа утра')
+        break;
+    case 5:
+        alert('5 часов утра')
+        break;
+    case 6:
+        alert('6 часов утра')
+        break;
+    case 7:
+        alert('7 часов утра')
+        break;
+    case 8:
+        alert('8 часов утра')
+        break;
+    case 9:
+        alert('9 часов утра')
+        break;
+    case 10:
+        alert('10 часов утра')
+        break;
+    case 11:
+        alert('11 часов утра')
+        break;
+    case 12:
+        alert('12 часов дня')
+        break;
+    case 13:
+        alert('час дня')
+        break;
+    case 14:
+        alert('2 часа дня')
+        break;
+    case 15:
+        alert('3 часа дня')
+        break;
+    case 16:
+        alert('4 часа дня')
+        break;
+    case 17:
+        alert('5 часа дня')
+        break;
+    case 18:
+        alert('6 часов вечера')
+        break;
+    case 19:
+        alert('7 часов вечера')
+        break;
+    case 20:
+        alert('8 часов вечера')
+        break;
+    case 21:
+        alert('9 часов вечера')
+        break;
+    case 22:
+        alert('10 часов ночи')
+        break;
+    case 23:
+        alert('11 часов ночи')
+        break;
+    case 24:
+        alert('12 часов ночи')
+        break;
+    default:
+        alert('Такого времени не существует')
+        break
+}
